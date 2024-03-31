47670b967589c63b5fb23f1bb3c71de107f8989b
lcruan <1416802331@qq.com>
2024-03-31 08:13:56 +0800

    ts函数声明和函数表达式写法

---
+++ 4、函数/js/1.函数声明和函数表达式.js
@@ -0,0 +1,29 @@
+/**js中 */
+// 函数声明，命名函数
+// function add(a, b) {
+//     return a + b
+// }
+// // 函数表达式，匿名函数
+// let add2 = function(a, b) {
+//     return a + b
+// }
+/**ts中 */
+// 1. ts 函数声明：命名函数
+// a 和 b 都是number类型
+// :number 表示的是该函数的返回值为number类型
+function add(a, b) {
+    return a + b;
+}
+console.log(add(1, 2));
+// 定义变量接受返回值
+let c = add(1, 2);
+console.log(c);
+// 2. 函数表达式，匿名函数 - 这种在ts并不是完整的写法, 没有给add2定义类型
+let add2 = function (a, b) {
+    return a + b;
+};
+console.log(add2(1, 2));
+// 3. 函数完整的写法
+let add3 = function (a, b) {
+    return a + b;
+};
